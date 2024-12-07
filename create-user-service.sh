a3a8f16b34047d370f1145fd493121d046e721e9
Jochen Roessner <jochen@zerties.org>
2024-12-07 19:47:45 +0100

    add simple service creation

---
+++ create-user-service.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+
+APP=bluetoothle_sensors
+START=monitor.py
+
+export START
+
+mkdir -p ~/.config/systemd/user
+
+cat $APP.service | envsubst | tee ~/.config/systemd/user/$APP.service
+
+systemctl --user enable $APP
+
+systemctl --user start $APP
+
+sudo loginctl enable-linger pi
+
+echo "journalctl --user -xfu $APP"
+
